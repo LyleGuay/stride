43d0d9fd3a9e61220d242ba95bc2902593364359
Lyle Guay <lyleguay@gmail.com>
2026-02-01 08:47:14 -0500

    Basic go api

--- go-api/main.go
+++ go-api/main.go
@@ -3,7 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type habit struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Cadence string `json:"cadence"`
+}
+
+var habits = []habit{
+	{Id: 1, Name: "Food (default or cook)", Cadence: "daily"},
+	{Id: 2, Name: "Calories (in budget or within 100)", Cadence: "daily"},
+}
+
+func getHabits(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, habits)
+}
+
+func postHabit(c *gin.Context) {
+	var newHabit habit
+
+	// Call BindJSON to bind the received JSON
+	if err := c.BindJSON(&newHabit); err != nil {
+		return
+	}
+
+	habits = append(habits, newHabit)
+	c.IndentedJSON(http.StatusCreated, newHabit)
+}
 
 func main() {
 	// Set properties of the predefined Logger, including
@@ -20,5 +50,13 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	fmt.Println("Hello World")
+	fmt.Println("Starting gin app...")
+
+	router := gin.Default()
+	router.SetTrustedProxies(nil)
+	//realIP := c.Request.RemoteAddr // This is the actual client - perfect!
+	router.GET("/habits", getHabits)
+	router.POST("/habits", postHabit)
+
+	router.Run("localhost:3000")
 }
--- go-api/plan-for-setting-up-go-api.md
+++ go-api/plan-for-setting-up-go-api.md
@@ -1,3 +1,19 @@
 
-- [ ] Get go app running
-- [ ] Add gin module
+# Basic setup
+My plan for setting up go api with postgres
+- [X] Get go app running
+- [X] Add gin module
+- [ ] Env vars (godotenv or viper)
+- [ ] Add postgres connection (pgx)
+- [ ] Create basic migration for habit table
+- [ ] Route to return habits
+
+# Lvl 2
+- [ ] Error handling middleware
+- [ ] Logging
+- [ ] Validation
+
+# Lv 3
+- [ ] Authentication/Authorization
+- [ ] Unit testing
+- [ ] Structured API Response
